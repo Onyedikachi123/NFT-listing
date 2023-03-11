34c3d57abb911f4f79bddd8801486c4c4d96f191
Onyedikachi123 <onyedikachierugo@gmail.com>
2023-03-11 04:17:52 +0100

    changed user address

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 import NFTList from './NFTList';
 
 function App() {
-  const userAddress = '0x1234567890'; // Replace with the user's address
+  const userAddress = '0xc986295b3212d9d78ff342f2656d5f331f72ff53'; // Replace with the user's address
 
   return (
     <Container>
